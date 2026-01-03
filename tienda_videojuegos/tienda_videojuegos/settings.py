aeb3fa392ccf04905e1097f829e388ed1a84c133
Aldair Andrade <jmaqueraandrade@gmail.com>
2026-01-03 15:35:09 -0500

    feat: App creadas home y catalogo

--- tienda_videojuegos/tienda_videojuegos/settings.py
+++ tienda_videojuegos/tienda_videojuegos/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'caalogo',
 ]
 
 ROOT_URLCONF = 'tienda_videojuegos.urls'
